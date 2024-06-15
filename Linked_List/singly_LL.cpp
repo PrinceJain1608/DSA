12e08e22e92a7d9ebc05f2563e7ac6e4461f492e
khushi-gunnu17 <princejain1608@gmail.com>
2024-06-15 17:04:53 +0530

    DSA

--- Linked_List/singly_LL.cpp
+++ Linked_List/singly_LL.cpp
@@ -5,9 +5,20 @@ class Node{
   int data;
   Node* next;
 
+  //constructor
   Node(int data){
     this->data=data;
     this->next=NULL;
+  }
+  //destructor
+  //memory free
+  ~Node(){
+    int value=this->data;
+    if(this->next!=NULL){
+      delete next;
+      this->next=NULL;
+    }
+    
   }
 };
 void insertAtHead(Node *&head,int d){
@@ -28,6 +39,54 @@ void print(Node *&head){
 
   }cout<<endl;
 }
+void insertAtPosition(Node *&tail,Node *&head,int position,int d){
+
+  //insert at start
+  if(position==1){
+    insertAtHead(head,d);
+    return;
+  }
+  Node *temp=head;
+  int cnt=1;
+  while(cnt<position-1){
+    temp=temp->next;
+    cnt++;
+  }
+  //insert at end
+  if(temp->next==NULL){
+    insertAtTail(tail,d);
+    return;
+  }
+  Node *nodeToInsert=new Node(d);
+  nodeToInsert->next=temp->next;
+  temp->next=nodeToInsert;
+}
+
+void deleteNode(Node *&head,int position){
+
+  //start element
+  if(position==1){
+    Node *temp=head;
+    temp->next=NULL;
+    delete temp;
+
+  }
+  //middle and last element
+  else{
+    Node *curr=head;
+    Node *prev=NULL;
+    int cnt=1;
+    while(cnt<position){
+      prev=curr;
+      curr=curr->next;
+      cnt++;
+    }
+    prev->next=curr->next;
+    curr->next=NULL;
+    delete curr;
+
+  }
+}
 int main(){
   Node *node1=new Node(10);
   // cout<<node1->data<<endl;
@@ -47,5 +106,12 @@ insertAtTail(tail,2);
 print(head);
 insertAtTail(tail,34);
 print(head);
-
+insertAtPosition(tail,head,3,99);
+print(head);
+insertAtPosition(tail,head,1,69);
+print(head);
+insertAtPosition(tail,head,7,0);
+print(head);
+deleteNode(head,3);
+print(head);
 }
