e90fb1101ea1bc89af509a0cf8933d23b7289af9
khushi-gunnu17 <princejain1608@gmail.com>
2024-07-03 19:07:27 +0530

    DSA

--- Trees/build_binary_tree.cpp
+++ Trees/build_binary_tree.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<queue>
 using namespace std;
 class Node{
   public:
@@ -15,7 +16,7 @@ Node(int d){
 
 Node *buildTree(Node *root){
   int data;
-  cout<<"Enter the data:";
+  cout<<"Enter the data:"<<endl;
   cin>>data;
   root=new Node(data);
   if(data==-1){
@@ -26,9 +27,36 @@ Node *buildTree(Node *root){
   cout<<"Enter data which has to be insert in the right of "<<data<<endl;
   root->right=buildTree(root->right);
   return root;
+}
 
+void levelOrderTraversal(Node *root){
+  queue<Node*> q;
+  q.push(root);
+  q.push(NULL);
+
+  while(!q.empty()){
+    Node *temp=q.front();
+    q.pop();
+    if(temp==NULL){
+      cout<<endl;
+      if(!q.empty()){
+        q.push(NULL);
+      }
+    }
+    else{
+      cout<<temp->data<<" ";
+      if(temp->left){
+        q.push(temp->left);
+      }
+      if(temp->right){
+        q.push(temp->right);
+      }
+    }
+  }
 }
 int main(){
   Node *root=NULL;
   root=buildTree(root);
+  cout << "Printing the level order traversal output " << endl;
+  levelOrderTraversal(root);
 }
