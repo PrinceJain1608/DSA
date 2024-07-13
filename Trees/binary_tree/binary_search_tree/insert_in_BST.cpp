169e9031bc537dc03fe20741a0c76292d843b4f9
khushi-gunnu17 <princejain1608@gmail.com>
2024-07-13 16:04:41 +0530

    DSA

--- Trees/binary_tree/binary_search_tree/insert_in_BST.cpp
+++ Trees/binary_tree/binary_search_tree/insert_in_BST.cpp
@@ -42,10 +42,77 @@ class Node{
     cout<<root->data<<" ";
     inOrder(root->right);
   }
+Node *minVal(Node *root){
+  Node *temp=root;
+  while(temp->left!=NULL){
+    temp=temp->left;
+  }
+  cout<<temp->data;
+}
+Node *maxVal(Node *root){
+  Node *temp=root;
+  while(temp->right!=NULL){
+    temp=temp->right;
+  }
+  cout<<temp->data;
+}
+Node *deleteFromBST(Node *root,int val){
+  //blank tree
+  if(root==NULL){
+    return root;
+  }
+  if(root->data==val){
+    //0 child
+    if(root->left==NULL && root->right==NULL){
+      delete root;
+      return NULL;
+    }
+    //one child
+    //left child
+        if(root->left!=NULL && root->right==NULL){
+          Node *temp=root->left;
+          delete root;
+          return temp;
+        }
+    //right child
+        if(root->left==NULL && root->right!=NULL){
+          Node *temp=root->right;
+          delete root;
+          return temp;
+        }
+    //two child
+        if(root->left!=NULL && root->right!=NULL){
+          int mini=minVal(root->right)->data;
+          root->data=mini;
+          root->right=deleteFromBST(root->right,mini);
+          return root;
+        }
+  }
+  else if(root->data>val){
+   root->left=deleteFromBST(root->left,val);
+   return root;
+  }
+  else{
+    root->right=deleteFromBST(root->right,val);
+    return root;
+  }
+  
+
+}
 int main(){
   Node *root=NULL;
   cout<<"Enter the data to crate BST:";
   takeInput(root);
+  cout<<"Printing BST:"<<endl;
+  inOrder(root);
+  cout<<endl;
+  cout<<"Min value in BST:";
+  minVal(root);
+  cout<<endl;
+  cout<<"Max value in BST:";
+  maxVal(root);
+  root=deleteFromBST(root,4);
+  cout<<endl;
   cout<<"Printing BST:"<<endl;
   inOrder(root);
   return 0;
